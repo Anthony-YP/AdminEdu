d7103e3da76749663f1e1b594ac41931f2436c1c
Anthony <anthony.j.yaguana@unl.edu.ec>
2026-07-12 21:08:03 -0500

    actualziacion admin

--- Backend/AdminEdu/gestion_academica/admin.py
+++ Backend/AdminEdu/gestion_academica/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 
-from .models.academia.Academia import Academia, Curso, Paralelo
-from .models.persona.Persona import Director, Docente, Secretaria, Estudiante, Representante
-from .models.matricula.Matricula import Matricula, Asistencia, CalificacionFinal
-from .models.notificaciones.Notificacion import Notificacion
-from .models.pagos.Pagos import ComprobantePago
+from .models.core.Core import *
+from .models.academia.Academia import *
+from .models.persona.Persona import *
+from .models.matricula.Matricula import *
+from .models.notificaciones.Notificacion import *
+from .models.pagos.Pagos import *
 
+admin.site.register(Direccion)
 admin.site.register(Academia)
 admin.site.register(Curso)
 admin.site.register(Paralelo)
+admin.site.register(Persona)
 admin.site.register(Director)
 admin.site.register(Docente)
 admin.site.register(Secretaria)
